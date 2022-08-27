18bceab8346f0b8235b7158b51b81f5de750ac8d
abhinavmishra01 <110123777+abhinavmishra01@users.noreply.github.com>
2022-08-27 13:19:07 +0530

    java

---
+++ Arthimeticopertaor.py
@@ -0,0 +1,26 @@
+from array import array
+from audioop import add
+from hashlib import new
+from inspect import _void
+from json.tool import main
+from os import system
+import string
+from xml.dom.minidom import Element
+from xmlrpc.client import Boolean, boolean
+import java.utill.ArrayList;
+import java .utill.Collections;
+class ArrayLists{
+    public state void main(string args[])
+    arraylist<Integer>list=new arraylist<integer>();
+    ArrayLists<string>list2=new arraylist<string>();
+    ArrayLists<Boolean>list3=new ArrayLists<boolean>;
+
+    //add Element
+    list.add(1);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    system.out.println(list);
+
+    // to get an Element
+}
---
+++ TWO NAMES.py
@@ -0,0 +1,20 @@
+
+name = "ABHINAV MISHRA"
+print(name.upper())
+print(name)
+
+print(name.lower())
+print(name)
+
+print(name.find('y'))
+print(name.find('Y'))
+print(name.find("Stark"))
+print(name.find("stark"))
+
+print(name.replace("ABHINAV MISHRA", "Ironman"))
+print(name)
+
+#to check if a character/string is part of the main string
+print("MISHRA" in name)
+print("S" in name)
+print("s" in name)
